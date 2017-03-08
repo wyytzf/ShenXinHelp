2244dad6f080e3a2a7e23a82f3a1cc21a4a68191
MMY <847171119@qq.com>
2017-03-08 09:58:26 +0800

    3-8

--- app/src/main/java/com/xd/shenxinhelp/com/xd/shenxinhelp/httpUtil/ConnectUtil.java
+++ app/src/main/java/com/xd/shenxinhelp/com/xd/shenxinhelp/httpUtil/ConnectUtil.java
@@ -45,6 +45,8 @@ public class ConnectUtil {
 //	public final static String API_HOST = "http://192.168.0.151:8080/ASEYiYi/";
 	public static String GetHomePageImages = API_HOST + "GetHomePageImages";
 	public static String GetExerciseItem = API_HOST + "GetExerciseItem";
+	public static String GetAllMyRing= API_HOST +"GetAllMyRing";
+	public static String GetRingMember= API_HOST +"GetRingMember";
 
 	public static boolean isNetworkAvailable(Context context) {
 		ConnectivityManager connect = (ConnectivityManager) context
